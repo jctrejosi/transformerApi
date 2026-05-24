65a631acd94d38c4f349c27aa1ffcdd624fc0faf
Juan Carlos Trejos <jctrejosi@unal.edu.co>
2026-05-24 17:46:46 -0500

    add cors

--- apis/main.py
+++ apis/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .upload_model import router as upload_router
 from .download_model import router as download_router
@@ -11,6 +12,17 @@ from .list_models import router as list_models
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://tu-frontend.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(upload_router)
 app.include_router(download_router)
 app.include_router(train_router)
@@ -19,4 +31,3 @@ app.include_router(fine_tuning_router)
 app.include_router(delete_all)
 app.include_router(delete_model)
 app.include_router(list_models)
-
